9cdd26bbc018cfe77bac62867dc1376176ef7683
andiharun29 <andiharunalrasyid14@gmail.com>
2024-11-23 20:01:34 +0700

    menambhakan database pada data bibit

---
+++ routes/bibit.js
@@ -0,0 +1,16 @@
+import express, { Router } from "express"; 
+const router = express.Router(); 
+const bibit = [ 
+    { 
+        id: "1", 
+        namabibit: "cabai", 
+        jenis: "sayuran", 
+        kadaluarsa: 2025, 
+    }, 
+    { 
+        id: "2", 
+        namabibit: "selada", 
+        jenis: "sayuran", 
+        kadaluarsa: 2022, 
+    }, 
+]; 
