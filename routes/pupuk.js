6c15cf7ce2635065d848d64eb8b4ebb91247f05f
andiharun29 <andiharunalrasyid14@gmail.com>
2024-11-23 19:59:57 +0700

    menambahkan perintah put pada data pupuk

--- routes/pupuk.js
+++ routes/pupuk.js
@@ -29,3 +29,21 @@ router.post('/', (req, res) => {
     res.status(201).json(newpupuk); 
 }); 
  
+router.put('/:id', (req, res) => { 
+    const pupukIndex = pupuk.findIndex(t => t.id === parseInt(req.params.id)); 
+    if (!pupukIndex === -1) 
+        return res.status(404).json({ message: 'Tugas tidak ditemukan' }); 
+ 
+    pupuk[pupukIndex] = { 
+        ...pupuk[pupukIndex], 
+        namapupuk: req.body.namapupuk || pupuk[pupukIndex].namapupuk, 
+        jenis: req.body.jenis || pupuk[pupukIndex].jenis, 
+        kandungan: req.body.kandungan || pupuk[pupukIndex].kandungan, 
+    }; 
+ 
+    res.status(200).json({ 
+        message: `Tugas dengan ID '${req.params.id}' telah diperbarui`, 
+        updatepupuk: pupuk[pupukIndex], 
+    }); 
+}); 
+ 
