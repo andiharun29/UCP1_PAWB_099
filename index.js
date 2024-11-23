07241f8083f37e8c8a6ff1e61fc4d52ca2ae5cf3
andiharun29 <andiharunalrasyid14@gmail.com>
2024-11-23 20:12:01 +0700

    Tambahkan setup server Express dasar dengan rute untuk bibit dan pupuk

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express"; 
+import bodyParser from "body-parser"; 
+import bibitRoute from "./routes/bibit.js"; 
+import pupukRoute from "./routes/pupuk.js"; 
+ 
+const app = express(); 
+const PORT = 8000; 
+ 
+app.use(bodyParser.json()); 
+ 
+app.use("/bibit", bibitRoute); 
+app.use("/pupuk", pupukRoute); 
+ 
+ 
