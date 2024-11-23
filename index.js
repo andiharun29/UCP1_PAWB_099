b3b125c869f009d3dc8c478ac2891c38d2a3317c
andiharun29 <andiharunalrasyid14@gmail.com>
2024-11-23 20:13:29 +0700

    Tambahkan rute GET dasar dan jalankan server dengan pesan log

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ app.use("/bibit", bibitRoute);
 app.use("/pupuk", pupukRoute); 
  
  
+app.get("/", (req, res) => { 
+    console.log(["GET ROUTE"]); 
+    res.send("Selamat Pagi"); 
+}); 
+ 
+app.use(bodyParser.json()); 
+app.listen(PORT, () => 
+    console.log(`Server berjalan di port : http://localhost:${PORT}`));
