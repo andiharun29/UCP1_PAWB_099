bd83a7fe1c27d608929efeb18886d63fc40a7164
andiharun29 <andiharunalrasyid14@gmail.com>
2024-11-23 19:54:34 +0700

    membuat database untuk data pupuk

---
+++ routes/pupuk.js
@@ -0,0 +1,15 @@
+import express, { Router } from "express"; 
+const router = express.Router(); 
+const pupuk = [ 
+    { 
+        namapupuk: "cabai", 
+        idpupuk: "1", 
+        jenis: "sayuran", 
+        kandungan: "beta karoten", 
+    }, 
+    { 
+        namapupuk: "selada", 
+        idpupuk: "2", 
+        jenis: "sayuran", 
+        kadaluars: "ke labu", 
+    }, 
